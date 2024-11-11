c5acfd39130b2fa97a2ff02ba8aa363f9a0fc298
kirangajjana <kiran.gajjana@gmail.com>
2024-11-11 19:04:16 +0530

    app.py updates

---
+++ app.py
@@ -0,0 +1,6 @@
+class Account:
+    "here i developed this class to memic basic bank functionality here"
+    bank_name="Kiran Bank of India"
+    def __init__(self):
+        print(f"Welcome to {self.bank_name}")
+a=Account()        
