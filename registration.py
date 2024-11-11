b6aa87401fd4de067b8f12fae857b408a199b37e
kirangajjana <kiran.gajjana@gmail.com>
2024-11-11 19:34:42 +0530

    added registration.py

---
+++ registration.py
@@ -0,0 +1,13 @@
+from app import Account as kiran
+class Registration:
+    def __init__(self,name,city):
+        self.name=name
+        self.city=city
+    def info(self):
+        print(f"welcome to {kiran.bank_name}")
+        print(f"hello mr. {self.name}")
+        print(f"your are from city {self.city}")
+name=input("please enter your name")
+city=input("please enter your city")
+b=Registration(name,city)     
+b.info()   
